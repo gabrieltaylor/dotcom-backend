1130136edc657f83fb60cfe5f225a388f1b5ebb1
Jonathan Soifer <me@jonathansoifer.com>
2016-11-05 09:28:34 -0200

    add Comment Model, references posts

---
+++ web/models/comment.ex
@@ -0,0 +1,20 @@
+defmodule Dotcom.Comment do
+  use Dotcom.Web, :model
+
+  schema "comments" do
+    field :name, :string
+    field :content, :string
+    belongs_to :post, Dotcom.Post
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :content])
+    |> validate_required([:name, :content])
+  end
+end
