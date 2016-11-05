eadf74519748685b13fe5d9bbed045e765560c58
Jonathan Soifer <me@jonathansoifer.com>
2016-11-05 19:31:46 -0200

    properly display comments

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -7,7 +7,9 @@ defmodule Dotcom.PostController do
   plug :scrub_params, "comment" when action in [:add_comment]
 
   def index(conn, _params) do
-    posts = Repo.all(Post)
+    posts = Post
+            |> Post.count_comments
+            |> Repo.all
     render(conn, "index.html", posts: posts)
   end
 
