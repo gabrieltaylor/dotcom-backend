3df1a48c0786f95124d5af4bd6b4db3010d9febd
Jonathan Soifer <me@jonathansoifer.com>
2016-11-06 03:22:57 -0200

    docker adjustments and script permissions

--- scripts/wait-for-postgres.sh
+++ scripts/wait-for-postgres.sh
@@ -7,11 +7,10 @@ shift
 cmd="$@"
 
 until psql -h "$host" -U "postgres" -c '\l'; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    >  sleep 1
-    >  done
-    >
-    >  >&2 echo "cmd arg: ${cmd}"
-    >  >>&2 echo "Postgres is up - entering app"
-    >  >>exec $cmd
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
 done
+
+>&2 echo "cmd arg: ${cmd}"
+>&2 echo "Postgres is up - entering app"
+exec $cmd
