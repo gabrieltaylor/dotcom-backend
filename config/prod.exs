6b711bf772016a058f4fbbd9b9d1bf2968f40455
Jonathan Soifer <me@jonathansoifer.com>
2016-11-06 12:40:40 -0200

    fixes copy/paste reference error

--- config/prod.exs
+++ config/prod.exs
@@ -65,7 +65,7 @@ config :logger, level: :info
 
 # Heroku Support
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :dotcom, Dotcom.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
