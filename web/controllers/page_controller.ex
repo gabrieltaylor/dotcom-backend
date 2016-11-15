9e9f7875d13e2c83a058b47cf0b958a750ea84c3
Jonathan Soifer <me@jonathansoifer.com>
2016-11-15 20:12:49 -0200

    fetch Page data available to View

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,16 @@
 defmodule Dotcom.PageController do
   use Dotcom.Web, :controller
 
+  alias Dotcom.Page
+
+  # Makes logging available
+  require Logger
+
   def index(conn, _params) do
-    render conn, "index.html"
+    query = from p in Page,
+      where: p.slug == "home",
+      select: p
+    page = Repo.one!(query)
+    render(conn, "index.html", page: page)
   end
 end
