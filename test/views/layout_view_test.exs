0282fd054e4b6bc60aea7e3967d18c66235a47ac
Jonathan Soifer <me@jonathansoifer.com>
2016-11-18 17:07:19 -0200

    add Session+Tests for Login and Logout
    add Session View
    
    add Login Screen / Session :new
    
    add Sessions resources [:new] to router
    
    create form to Session [:new] html page
    
    add Login form to Session [:new] View
    
    pass changeset as a template variable to User:new
    
    allow Session[:create] on router
    
    add Session Controller logic to login (or not)
    
    refactor Session Controller sign_in
    
    add helper to Layout view: check session
    
    add Session Controller tests
    
    add Logged in / Session info to Homepage
    
    add [:delete] to Session resource in router
    
    add :delete to session controller
    
    add view tests to Session
    
    update layout views test

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -1,3 +1,37 @@
 defmodule Dotcom.LayoutViewTest do
   use Dotcom.ConnCase, async: true
+
+  alias Dotcom.LayoutView
+  alias Dotcom.User
+
+  setup do
+    User.changeset(%User{}, %{username: "test",
+                              password: "test",
+                              password_confirmation: "test",
+                              email: "test@test.com",
+                              first_name: "Test",
+                              last_name: "Tested"})
+    |> Repo.insert
+    {:ok, conn: build_conn()}
+  end
+
+  test "current user returns the user in session", %{conn: conn} do
+    conn = post conn, session_path(conn, :create), user: %{username: "test",                                                           password: "test"}
+    assert LayoutView.current_user(conn)
+  end
+
+  test "current user return nothing is there is no user in the session", %{conn: conn} do
+    user = Repo.get_by(User, %{username: "test"})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute LayoutView.current_user(conn)
+  end
+
+  test "deletes the user session", %{conn: conn} do
+    user = Repo.get_by(User, %{username: "test"})
+    conn = delete conn, session_path(conn, :delete, user)
+    refute get_session(conn, :current_user)
+    assert get_flash(conn, :info) == "Signed out successfully!"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
 end
