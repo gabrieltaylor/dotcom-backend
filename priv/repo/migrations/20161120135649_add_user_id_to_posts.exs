a342b93dfce16608ef46da41f233f9794e735437
Jonathan Soifer <me@jonathansoifer.com>
2016-11-20 12:00:33 -0200

    describe the actual add_user_id_to_posts migration

--- priv/repo/migrations/20161120135649_add_user_id_to_posts.exs
+++ priv/repo/migrations/20161120135649_add_user_id_to_posts.exs
@@ -2,6 +2,9 @@ defmodule Dotcom.Repo.Migrations.AddUserIdToPosts do
   use Ecto.Migration
 
   def change do
-
+    alter table(:posts) do
+      add :user_id, references(:users)
+    end
+    create index(:posts, [:user_id])
   end
 end
