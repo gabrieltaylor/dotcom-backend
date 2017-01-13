31b4d53414212f7fcb63738c1cea3232756b9d5d
Jonathan Soifer <me@jonathansoifer.com>
2017-01-13 21:24:05 -0200

    delete legacy tests for Page/User/Session

--- test/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Dotcom.PageControllerTest do
-  use Dotcom.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
--- test/views/page_view_test.exs
+++
@@ -1,3 +0,0 @@
-defmodule Dotcom.PageViewTest do
-  use Dotcom.ConnCase, async: true
-end
