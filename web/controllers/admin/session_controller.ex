7cce161a38ed98c19b4028a60342b39ee07f6f28
Jonathan Soifer <me@jonathansoifer.com>
2017-01-18 19:21:57 -0200

    Add Helper function for View (logged_in?)
    Make Helper Function available for Views
    
    fix session's View Helpers call and location

--- web/controllers/admin/session_controller.ex
+++ web/controllers/admin/session_controller.ex
@@ -22,4 +22,11 @@ defmodule Dotcom.Admin.SessionController do
     end
   end
 
+  def current_user(conn) do
+    id = Plug.Conn.get_session(conn, :current_user)
+    if id, do: Dotcom.Repo.get(User,id)
+  end
+
+  def logged_in?(conn), do: !!current_user(conn)
+
 end
