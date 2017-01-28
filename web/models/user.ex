91146047678229eb72b72096f5e6c6edeb49c749
Jonathan Soifer <me@jonathansoifer.com>
2017-01-28 16:02:29 -0200

    Refactor from Session Helper into User Model

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,5 +1,6 @@
 defmodule Dotcom.User do
   use Dotcom.Web, :model
+  alias Dotcom.User
 
   schema "users" do
 
@@ -26,6 +27,26 @@ defmodule Dotcom.User do
     |> unique_constraint(:email)
     |> validate_length(:password, min: 5)
     |> validate_format(:email, ~r/@/)
+  end
+
+  def login(params, repo) do
+    user = repo.get_by(User, email: String.downcase(params["email"]))
+    case authenticate(user, params["password"]) do
+      true -> {:ok, user}
+      _    -> :error
+    end
+  end
+
+  defp authenticate(user, password) do
+    case user do
+      nil -> false
+      _   -> Comeonin.Bcrypt.checkpw(password, user.crypted_password)
+    end
+  end
+
+  def current_user(conn) do
+    id = Plug.Conn.get_session(conn, :current_user)
+    if id, do: Dotcom.Repo.get(User,id)
   end
 
 end
